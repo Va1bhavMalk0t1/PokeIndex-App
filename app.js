c4463658ba1f7c0c09aeb2f3fa3901db471f79d2
Vaibhav Malkoti <177619278+Va1bhavMalk0t1@users.noreply.github.com>
2025-02-12 23:31:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,16 +10,22 @@ final.style.display = 'none';
 button.addEventListener('click' , () => {
     const pokemon = input.value.toLowerCase(); 
     pokeApi(pokemon) ; 
-    final.style.display = 'block';
-    input.value = '';
 }) ; 
 
 
 const pokeApi = async (pokemon) => {
+    try {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const data = await response.json();
     console.log(data);
     img.src = data.sprites.front_shiny;
     nameofPokemon.innerText = `Name: ${data.name.toUpperCase()}`;
     type.innerText = `Type: ${data.types[0].type.name.toUpperCase()}`;
+    final.style.display = 'block';
+    input.value = '';
+    } catch(e) {
+        alert("Please Enter Valid Pokemon Name !!!!");
+        console.log(e) ; 
+        input.value = '';
+    }
 }
